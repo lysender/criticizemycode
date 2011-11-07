f53cecfe9a49412a36e217840c18553c10f11f30
Lysender <dc.eros@gmail.com>
2011-11-07 18:37:45 +0800

    Off to pagoda

--- application/config/database.sample.php
+++ application/config/database.sample.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php defined('SYSPATH') or die('No direct access allowed.');
 
 return array
 (
@@ -17,34 +17,36 @@ return array
 			 * boolean  persistent
 			 * string   database
 			 */
-			'dsn'		 => 'mysql:host=localhost;dbname=simplestore',
-			'username'   => 'simplestore',
-			'password'   => 'simplestore',
+			'dsn'		 => 'mysql:host=localhost;dbname=cmc',
+			'username'   => 'cmc',
+			'password'   => 'cmc',
 			'persistent' => FALSE,
 		),
-		'table_prefix' => '',
+		'table_prefix' => 'cmc_',
 		'charset'      => 'utf8',
 		'caching'      => FALSE,
-		'profiling'    => FALSE,
+		'profiling'    => TRUE,
 	),
-	'development' => array
+	'alternate' => array
 	(
-		'type'       => 'pdo',
+		'type'       => 'mysql',
 		'connection' => array(
 			/**
 			 * The following options are available for MySQL:
 			 *
-			 * string   hostname
-			 * integer  port
-			 * string   socket
-			 * string   username
-			 * string   password
-			 * boolean  persistent
-			 * string   database
+			 * string   hostname     server hostname, or socket
+			 * string   database     database name
+			 * string   username     database username
+			 * string   password     database password
+			 * boolean  persistent   use persistent connections?
+			 * array    variables    system variables as "key => value" pairs
+			 *
+			 * Ports and sockets may be appended to the hostname.
 			 */
-			'dsn'		 => 'mysql:host=localhost;dbname=simplestore',
-			'username'   => 'simplestore',
-			'password'   => 'simplestore',
+			'hostname'   => 'localhost',
+			'database'   => 'cmc',
+			'username'   => 'cmc',
+			'password'   => 'cmc',
 			'persistent' => FALSE,
 		),
 		'table_prefix' => '',
@@ -52,54 +54,4 @@ return array
 		'caching'      => FALSE,
 		'profiling'    => TRUE,
 	),
-	'testing' => array
-	(
-		'type'       => 'pdo',
-		'connection' => array(
-			/**
-			 * The following options are available for MySQL:
-			 *
-			 * string   hostname
-			 * integer  port
-			 * string   socket
-			 * string   username
-			 * string   password
-			 * boolean  persistent
-			 * string   database
-			 */
-			'dsn'		 => 'mysql:host=localhost;dbname=simplestore',
-			'username'   => 'simplestore',
-			'password'   => 'simplestore',
-			'persistent' => FALSE,
-		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-		'profiling'    => TRUE,
-	),
-	'staging' => array
-	(
-		'type'       => 'pdo',
-		'connection' => array(
-			/**
-			 * The following options are available for MySQL:
-			 *
-			 * string   hostname
-			 * integer  port
-			 * string   socket
-			 * string   username
-			 * string   password
-			 * boolean  persistent
-			 * string   database
-			 */
-			'dsn'		 => 'mysql:host=localhost;dbname=simplestore',
-			'username'   => 'simplestore',
-			'password'   => 'simplestore',
-			'persistent' => FALSE,
-		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-		'profiling'    => FALSE,
-	)
 );
