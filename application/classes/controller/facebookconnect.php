142419bf2e712725232be0b2ad994e059780471b
Lysender <dc.eros@gmail.com>
2011-11-14 18:58:48 +0800

    Playing around with fb connect

--- application/classes/controller/facebookconnect.php
+++ application/classes/controller/facebookconnect.php
@@ -4,9 +4,45 @@ class Controller_Facebookconnect extends Controller_Site {
 	
 	public $auto_render = FALSE;
 
+	protected $_facebook;
+
+	public function before()
+	{
+		parent::before();
+
+		if ( ! class_exists('Facebook'))
+		{
+			include Kohana::find_file('vendor', 'facebook/src/facebook');
+		}
+
+		$config = Kohana::$config->load('facebook');
+
+		$this->_facebook = new Facebook(array(
+			'appId' => $config['app_id'],
+			'secret' => $config['secret'],
+			'cookie' => $config['cookie']
+		));
+
+
+	}
+
 	public function action_index()
 	{
 		var_dump($_GET);
+		$user = $this->_facebook->getUser();
+
+		if ( ! $user)
+		{
+			$this->request->redirect($this->_facebook->getLoginUrl(array('scope' => 'email')));
+		}
+		else
+		{
+			var_dump($user);
+
+			$profile = $this->_facebook->api('/me');
+			var_dump($profile);
+		}
+		var_dump($_COOKIE, $_SESSION);
 	}
 
 	public function action_connect()
@@ -27,7 +63,7 @@ class Controller_Facebookconnect extends Controller_Site {
 		$config = Kohana::$config->load('facebook');
 		$land = URL::site('/facebookconnect', TRUE);
 		$code = 'AQCn-4nxRnsR7wY1ozc-8rrP3C3bnjFUXm0FXk62DWrqVAVlsjb4qWyZ8kdJa_1HjzMzcJHwcDisFYCnY3ylUp3FtmHcM9P8bb4BF4t_hyI_bOf8EVrQlg_jDsI9iRtITSdG7A6YezuD-oO6qLCA9Hyg7iCx2arltx69L6ZfTyCAxlmMrXN7Q72Vp0BFUjte9hQ';
-		
+
 		$fburl = sprintf($url, $config['app_id'], urlencode($land), $config['app_secret'], $code);
 		var_dump($fburl);
 	}
--- application/config/facebook.php
+++ application/config/facebook.php
@@ -2,5 +2,6 @@
 
 return array(
 	'app_id' => '242571022466795',
-	'app_secret' => 'bb5c66bb2a627312e47953e89031622a'
+	'secret' => 'bb5c66bb2a627312e47953e89031622a',
+	'cookie' => TRUE
 );
