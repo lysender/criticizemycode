4353ff7c2c02643e3dd9835534973f470e342a76
Lysender <dc.eros@gmail.com>
2011-11-14 13:54:23 +0800

    Finalizing 0.2.0 branch

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -74,7 +74,7 @@ define('GENERIC_SALT', '5uctzl4g1VdaZmR4opZ3SvWCXeTvwqW5');
 /**
  * Defines the version of the application
  */
-define('APP_VERSION', '0.2.0-dev-01');
+define('APP_VERSION', '0.2.0');
 
 /**
  * Initialize Kohana, setting the default options.
